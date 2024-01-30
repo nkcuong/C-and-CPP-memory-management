7d27f3445abf7e348b000942dcd4c436ea7061c5
nkcuong <nguyenkcuong@gmail.com>
2024-01-30 16:58:34 +0700

    socket and ajax version of production consumer

--- ProducerConsumerOOP.py
+++ ProducerConsumerOOP.py
@@ -4,80 +4,86 @@ import time
 import random
 from queue import Empty
 
-
-
-class Producer(threading.Thread):
-	def __init__(self, pcq, n, velocity):
-		threading.Thread.__init__(self)
-		self.pcq = pcq
-		self.n = n
-		self.counter = 0
-		self.velocity = velocity
-
-	def run(self):
-		while self.counter < 5:
-			time.sleep(self.velocity)
-			i = random.randint(1, 1000)
-			self.pcq.produce(i, self.n)
-			self.counter += 1
-
-
-class Consumer(threading.Thread):
-	def __init__(self, pcq, m, velocity):
-		threading.Thread.__init__(self)
-		self.pcq = pcq
-		self.m = m
-		self.counter = 0
-		self.velocity = velocity
-
-	def run(self):
-		while self.counter < 10:
-			time.sleep(self.velocity)
-			self.pcq.consume(self.m)
-			# if no more to consume, break
-			self.counter += 1
-
-
 class ProducerConsumerQueue:
 	def __init__(self, capacity):
 		self.queue = queue.Queue(capacity)
 		print("Queue created with capacity: " + str(capacity) + "\n")
 
-	def produce(self, item, n):
+	def produce(self, item):
 		self.queue.put(item)  # Blocks if queue is full
-		print("Producer " + str(n) + " produced: " + str(item) + " - queue: " + str(self.queue.queue) + "\n")
 	
-
-	def consume(self, n):
+	def consume(self):
 		try:
-			item = self.queue.get(block=False)  # Raises queue.Empty if queue is empty
-			print("Consumer " + str(n) + " consumed: " + str(item) + " - queue: " + str(self.queue.queue) + "\n")
+			# item = self.queue.get(block=False)  # Raises queue.Empty if queue is empty
+			item = self.queue.get(block=True)
 			return item
 		except Empty:
-			print("Consumer " + str(n) + " stopping: no more items to consume")
+			print("Consumer " + self.name + "stopping: no more items to consume")
 			return None
 
 
-def start_producers():
-	global producers
-	producers = [Producer(pcq, n, 1) for n in range(1, 5)] # 4 producers with velocity 1
-	for producer in producers:
-		producer.start()
+class Producer(threading.Thread):
+	def __init__(self, name, pcq, capacity, velocity):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.pcq = pcq
+		self.capacity = capacity
+		self.counter = 0
+		self.velocity = velocity
+		self.lastProduced = None
+
+	def run(self):
+		while self.counter < self.capacity:
+			time.sleep(self.velocity)
+			i = random.randint(1, 1000)
+			self.pcq.produce(i)
+			self.lastProduced = i
+			print("Producer " + self.name + " produced: " + str(i) + " - queue: " + "len:" + str(self.pcq.queue.qsize()) + " - Content: " + str(self.pcq.queue.queue) + "\n")
+			self.counter += 1
 
 
-def start_consumers():
-	global consumers
-	consumers = [Consumer(pcq, m, 2) for m in range(1, 3)] # 2 consumers with velocity 2
-	for consumer in consumers:
-		consumer.start()
+class Consumer(threading.Thread):
+	def __init__(self, name, pcq, capacity, velocity):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.pcq = pcq
+		self.capacity = capacity
+		self.counter = 0
+		self.velocity = velocity
+		self.lastConsumed = None
+
+	def run(self):
+		while self.counter < self.capacity:
+			time.sleep(self.velocity)
+			i = self.pcq.consume()
+			self.lastConsumed = i
+			print("Consumer " + self.name + " consume: " + str(i) + " - queue: " + str(self.pcq.queue.queue) + "\n")
+			# if no more to consume, break
+			self.counter += 1
+
+
+# test the whole thing
+# main: uncomment following lines:
+			
+# def start_producers():
+# 	global producers
+# 	producers = [Producer(str(n), pcq, 100, 2) for n in range(1, 5)] # 4 producers with velocity 1
+# 	for producer in producers:
+# 		producer.start()
+
+
+# def start_consumers():
+# 	global consumers
+# 	consumers = [Consumer(str(m), pcq, 100, 1.5) for m in range(1, 3)] # 2 consumers with velocity 2
+# 	for consumer in consumers:
+# 		consumer.start()
 
 
 
-	
-pcq = ProducerConsumerQueue(100)
-start_producers()
-time.sleep(2)  # Let the producers produce for 2 seconds
-start_consumers()
+# pcq = ProducerConsumerQueue(100)
+# start_producers()
+# time.sleep(2)  # Let the producers produce for 2 seconds
+# start_consumers()
 
 
 
--- monitor.py
+++ monitor.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO, emit
+from ProducerConsumerOOP import ProducerConsumerQueue, Producer, Consumer
+import threading
+import time
 
 app = Flask(__name__)
-pcq = ProducerConsumerQueue(5)
-producer = Producer(pcq, "Producer")
-consumer = Consumer(pcq, "Consumer")
+socketio = SocketIO(app)
+pcq = ProducerConsumerQueue(100)
+producer1 = Producer("P1", pcq, 1000, 5)
+producer2 = Producer("P2", pcq, 1000, 3)
+consumer1 = Consumer("C1", pcq, 1000, 6)
+consumer2 = Consumer("C2", pcq, 1000, 4)
 
 @app.route('/')
 def index():
@@ -13,7 +20,22 @@ def index():
 def queue():
     return jsonify(list(pcq.queue.queue))
 
+@socketio.on('start')
+def handle_start():
+    threading.Thread(target=monitor).start()
+    producer1.start()
+    producer2.start()
+    time.sleep(5)
+    consumer1.start()
+    consumer2.start()
+    
+
+def monitor():
+    while True:
+        socketio.emit('queue', "Length: "+ str(pcq.queue.qsize()) + str(list(pcq.queue.queue)))
+        socketio.emit('producers', str(producer1.name) + " produced: " + str(producer1.lastProduced) + " counter : " + str(producer1.counter) + " -- " + str(producer2.name) + " produced: " + str(producer2.lastProduced) + " counter :  " + str(producer2.counter))
+        socketio.emit('consumers', str(consumer1.name) + " consumed: " + str(consumer1.lastConsumed) + " counter : " + str(consumer1.counter)  + " -- " + str(consumer2.name) + " consumed: " + str(consumer2.lastConsumed) +  " counter : " + str( consumer2.counter))
+        time.sleep(0.5)
+
 if __name__ == '__main__':
-    producer.start()
-    consumer.start()
-    app.run(debug=True)
+    socketio.run(app, debug=True)
